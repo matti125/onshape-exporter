3f500c49370104f2a413000aeaf085ce24823834
matti125 <none@none.invalid>
2025-06-05 16:44:16 +0300

    added "ctx" as a single parameter to pass API calling function.

--- onshape-exporter.py
+++ onshape-exporter.py
@@ -10,6 +10,12 @@ import argparse
 from urllib.parse import quote
 
 
+class Context:
+    def __init__(self, access, secret, ids):
+        self.auth = (access, secret)
+        self.ids = ids
+
+
 def load_api_keys(path):
     with open(path) as f:  # Change file name if needed
         data = json.load(f)
@@ -24,32 +30,44 @@ def get_ids(url):
     eIndex = urlArr.index("e")
     EID = urlArr[eIndex + 1]
 
-    if "v" in urlArr:
-        idx = urlArr.index("v")
-        VWID = urlArr[idx + 1]
-        path = f"v/{VWID}"
-    elif "w" in urlArr:
-        idx = urlArr.index("w")
-        VWID = urlArr[idx + 1]
-        path = f"w/{VWID}"
-    else:
-        raise ValueError("URL must contain either a 'w' (workspace) or 'v' (version)")
-
     MID = None
     if "m" in urlArr:
         mIndex = urlArr.index("m")
         MID = urlArr[mIndex + 1]
-        path += f"/m/{MID}"
 
-    return DID, EID, VWID, path
+    VWID = None
+    if "v" in urlArr:
+        idx = urlArr.index("v")
+        VWID = urlArr[idx + 1]
+    elif "w" in urlArr:
+        idx = urlArr.index("w")
+        VWID = urlArr[idx + 1]
+    else:
+        raise ValueError("URL must contain either a 'w' (workspace) or 'v' (version)")
+
+    return {
+        'did': DID,
+        'eid': EID,
+        'vwid': VWID,
+        'mid': MID,
+        'wvm': 'm' if MID else ('v' if 'v' in urlArr else 'w')
+    }
 
 
+def parse_onshape_path(ids, template):
+    # ids is a dict with keys: did, eid, wvid, wvm (optional)
+    # wvm is the workspace/microversion part like "w/12345" or "m/67890"
+    # wvid is the ID without prefix
+    # did and eid are document and element IDs
+    # template is a string with placeholders {did}, {eid}, {wvid}, {wvm}
+    return f"https://cad.onshape.com/api{template.format(did=ids['did'], eid=ids['eid'], wvid=ids['vwid'], wvm=ids['wvm'])}"
 
-def get_configuration_schema(api_access, api_secret, DID, VWM_PATH, EID):
-    url = f"https://cad.onshape.com/api/elements/d/{DID}/{VWM_PATH}/e/{EID}/configuration"
+
+def get_element_configuration(ctx):
+    url = parse_onshape_path(ctx.ids, "/elements/d/{did}/{wvm}/{wvid}/e/{eid}/configuration")
     response = requests.get(
         url,
-        auth=(api_access, api_secret),
+        auth=ctx.auth,
         headers={"Accept": "application/json"}
     )
     response = response.json()
@@ -74,16 +92,14 @@ def resolve_configuration_parameters(config, config_schema):
 
     resolved_params = []
     for conf_param_name, conf_value in config.items():
-        # check if the 
+        # check that the UI -visible name is in the schema a key
         if conf_param_name not in config_schema:
             raise ValueError(f"Unknown configuration parameter: '{conf_param_name}'")
 
         conf_param_def = config_schema[conf_param_name]["message"]
         param_id = conf_param_def["parameterId"]
         conf_param_type = config_schema[conf_param_name].get("typeName")
-        # if conf_param_type == "BTMConfigurationParameterQuantity":
-        #     print(f"DEBUG: conf_param_name={conf_param_name}, conf_value={conf_value}, units={conf_param_def.get('units')}")
-
+ 
         if conf_param_type == "BTMConfigurationParameterEnum":
             valid_options = {opt["message"]["optionName"]: opt["message"]["option"] for opt in conf_param_def["options"]}
             if conf_value not in valid_options:
@@ -132,13 +148,11 @@ def resolve_configuration_parameters(config, config_schema):
     return resolved_params
 
 
-def get_part_id(api_access, api_secret, DID, VWM_PATH, EID, partToExport, queryParam):
-
-    print(f"qp: {queryParam}")
-    url = f"https://cad.onshape.com/api/parts/d/{DID}/{VWM_PATH}/e/{EID}?{queryParam}"
+def get_part_id(ctx, partToExport, queryParam):
+    url = parse_onshape_path(ctx.ids, "/parts/d/{did}/{wvm}/{wvid}/e/{eid}") + f"?{queryParam}"
     response = requests.get(
         url,
-        auth=(api_access, api_secret),
+        auth=ctx.auth,
         headers={
             "Accept": "application/json"
         }
@@ -152,16 +166,11 @@ def get_part_id(api_access, api_secret, DID, VWM_PATH, EID, partToExport, queryP
     raise ValueError(f"Part named '{partToExport}' not found in the Part Studio.")
 
 
-def encode_configuration(api_access, api_secret, DID, EID, os_config_parameters):
-    #os_config_parameters is the onshape configuration parameters that have been resolved from UI visible values to 
-    #onshape-internal values like 
-    # [
-    #  {'parameterId': 'customSlotsX', 'parameterValue': '3'}, 
-    #  {'parameterId': 'List_Vbx6zoWJxuPf8k', 'parameterValue': 'Default'} 
-    # ]
+def encode_configuration_url(ctx, os_config_parameters):
+    url = parse_onshape_path(ctx.ids, "/elements/d/{did}/e/{eid}/configurationencodings")
     response = requests.post(
-        f"https://cad.onshape.com/api/elements/d/{DID}/e/{EID}/configurationencodings",
-        auth=(api_access, api_secret),
+        url,
+        auth=ctx.auth,
         headers={
             "Accept": "application/json;charset=UTF-8; qs=0.09",
             "Content-Type": "application/json"
@@ -178,20 +187,12 @@ def encode_configuration(api_access, api_secret, DID, EID, os_config_parameters)
     return encodedId, queryParam
 
 
-def start_translation(api_access, api_secret, DID, VWM_PATH, EID, encodedId, PID, formatName="STEP"):
-    # print(f"DID WID EID PID {DID} {WID} {EID} {PID}")
-    # try:
-    #     decoded_bytes = base64.urlsafe_b64decode(encodedId + '==')  # Add padding if needed
-    #     decoded_str = decoded_bytes.decode('utf-8')
-    #     print("Decoded config:", decoded_str)
-    # except Exception as e:
-    #     print(f"Failed to decode config: {e}")
-
-    url = f"https://cad.onshape.com/api/partstudios/d/{DID}/{VWM_PATH}/e/{EID}/translations"
+def create_translation_request(ctx, encodedId, PID, formatName="STEP"):
+    url = parse_onshape_path(ctx.ids, "/partstudios/d/{did}/{wvm}/{wvid}/e/{eid}/translations")
 
     response = requests.post(
         url,
-        auth=(api_access, api_secret),
+        auth=ctx.auth,
         headers={
             "Accept": "application/json;charset=UTF-8; qs=0.09",
             "Content-Type": "application/json"
@@ -207,11 +208,11 @@ def start_translation(api_access, api_secret, DID, VWM_PATH, EID, encodedId, PID
     return response.json()['id']
 
 
-def wait_for_translation(api_access, api_secret, TID):
+def wait_for_translation_request(ctx, TID):
     while True:
         response = requests.get(
             f"https://cad.onshape.com/api/translations/{TID}",
-            auth=(api_access, api_secret),
+            auth=ctx.auth,
             headers={
                 "Accept": "application/json;charset=UTF-8; qs=0.09",
                 "Content-Type": "application/json"
@@ -226,10 +227,11 @@ def wait_for_translation(api_access, api_secret, TID):
     return translation_status
 
 
-def download_result(api_access, api_secret, DID, FID, filename="result.step"):
+def download_external_data(ctx, FID, filename="result.step"):
+    url = f"https://cad.onshape.com/api/documents/d/{ctx.ids['did']}/externaldata/{FID}"
     response = requests.get(
-        f"https://cad.onshape.com/api/documents/d/{DID}/externaldata/{FID}",
-        auth=(api_access, api_secret),
+        url,
+        auth=ctx.auth,
         headers={
             "Accept": "application/octet-stream",
             "Content-Type": "application/json"
@@ -244,18 +246,39 @@ def load_config_with_fallback(filename="onshape-exporter.conf"):
         sys.exit(1)
     with open(filename) as f:
         return json.load(f)
+    
+def get_version_name(ctx):
+    url = parse_onshape_path(ctx.ids, "/documents/d/{did}/versions")
+    response = requests.get(url, auth=ctx.auth, headers={"Accept": "application/json"})
+    if response.status_code == 200:
+        version_info_list = response.json()
+        version_entry = next((v for v in version_info_list if v["id"] == ctx.ids["vwid"]), None)
+        return version_entry["name"] if version_entry else ctx.ids["vwid"]
+    else:
+        print(response.text)
+        return ctx.ids["vwid"]
+
+def generate_file_suffix(ctx):
+    suffix = ""
+    if ctx.ids['wvm'] == "v":
+        suffix += get_version_name(ctx)
+    else:
+        suffix += "wip"
+
+    if ctx.ids['wvm'] == "m":
+        shortid = ctx.ids['mid'][:8]
+        suffix += f"-m-{shortid}"
+    return suffix
 
 def main():
     parser = argparse.ArgumentParser(description="Export Onshape configurations")
     parser.add_argument("--config", help="Path to configuration file", default="onshape-exporter.conf")
-    parser.add_argument("--keyfile", help="Path to Onshape API key file", default="~/Onshape-test-APIKey.json")
+    parser.add_argument("--keyfile", help="Path to Onshape API key file", default=os.path.expanduser("~/Onshape-test-APIKey.json"))
     parser.add_argument("--url", help="Override the URL in config")
     parser.add_argument("--part", help="Override the part name in config")
     args = parser.parse_args()
 
-    path = os.path.expanduser(args.keyfile)
-    API_ACCESS, API_SECRET = load_api_keys(path)
-
+    API_ACCESS, API_SECRET = load_api_keys(args.keyfile)
     config_data = load_config_with_fallback(args.config)
 
     if args.url:
@@ -264,33 +287,18 @@ def main():
         config_data["part"] = args.part
 
     # read the config file
-    URL = config_data["url"]
     formatName = config_data.get("format", "STEP")
     partName = config_data.get("part", "Part 1")
     configurationsToExport = config_data.get("configurationsToExport", [])
 
-    #Get Document, Element (tab) and workspace/version and microversion IDs from URL
-    # VWM_PATH includes the microversion if there is one
-    DID, EID, VWID, VWM_PATH = get_ids(URL)
+    # the context include authentication and the document id, version/workspace/microversion IDs
+    ctx = Context(API_ACCESS, API_SECRET, get_ids(config_data["url"]))
 
-    version_tag = "wip"
-    if "/v/" in URL:
-        version_tag = VWID
-        try:
-            version_response = requests.get(
-                f"https://cad.onshape.com/api/documents/d/{DID}/versions",
-                auth=(API_ACCESS, API_SECRET),
-                headers={"Accept": "application/json"}
-            )
-            version_response.raise_for_status()
-            versions = version_response.json()
-            version_name = next((v["name"] for v in versions if v["id"] == VWID), VWID)
-            version_tag = version_name
-        except Exception as e:
-            print(f"Warning: could not resolve version name: {e}")
+    suffix = generate_file_suffix(ctx)
+
     # get the onshape-internal configuration schema values that API will mostly use 
     # instead of the GUI-visible names
-    config_schema = get_configuration_schema(API_ACCESS, API_SECRET, DID, VWM_PATH, EID)
+    config_schema = get_element_configuration(ctx)
 
     # Validate all configurations first
     for export in configurationsToExport:
@@ -305,26 +313,21 @@ def main():
         print(json.dumps(export))
         #map parameters from GUI values to onshape-internally used items
         resolved_params = resolve_configuration_parameters(export["config"], config_schema)
-
         #Encode the parameters so that the configuration can be used in POST or GET operations to the API
-        encodedId, queryParam = encode_configuration(API_ACCESS, API_SECRET, DID, EID, resolved_params)
-        # print(encodedId)
-        # print(queryParam)
-        #find the internal part Id. Pass the product configuration as well, as that can change the internal names
-        PID = get_part_id(API_ACCESS, API_SECRET, DID, VWM_PATH, EID, partName, queryParam=queryParam)
-        # print(f"PID: {PID}")
-        
-        TID = start_translation(API_ACCESS, API_SECRET, DID, VWM_PATH, EID, encodedId, PID, formatName=formatName)
-        # print(f"Started translation with ID: {TID}")
-
-        translation_status = wait_for_translation(API_ACCESS, API_SECRET, TID)
-    #    print(json.dumps(translation_status, indent=2))
+        encodedId, queryParam = encode_configuration_url(ctx, resolved_params)
+        #find the internal part Id. Pass the element configuration as well with queryParam, as that can change the internal names
+        PID = get_part_id(ctx, partName, queryParam=queryParam)
+        #start the translation
+        TID = create_translation_request(ctx, encodedId, PID, formatName=formatName)
+        # poll until translation ready
+        translation_status = wait_for_translation_request(ctx, TID)
 
         FID = translation_status['resultExternalDataIds'][0]
 
-        filename = f"{partName}-{export['name']}-{version_tag}.{formatName.lower()}"
-        download_result(API_ACCESS, API_SECRET, DID, FID, filename=filename)
+        filename = f"{partName}-{export['name']}-{suffix}.{formatName.lower()}"
+        download_external_data(ctx, FID, filename=filename)
 
 
 if __name__ == "__main__":
     main()
+
